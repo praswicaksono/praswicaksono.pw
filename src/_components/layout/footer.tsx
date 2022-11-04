e13884772b8121216c9654c977c6a720f3ad374b
Prasetyo Wicaksono <pras.wicak92@gmail.com>
2022-11-04 23:23:49 +0700

    fix: styling

--- src/_components/layout/footer.tsx
+++ src/_components/layout/footer.tsx
@@ -4,7 +4,8 @@ export default ({ comp, site }: PageData, { urlFilter }: PageHelpers) => {
   return (
     <footer className="container">
       <p>
-        designed & built by <a href="https://jrson.me" target="_blank">jrson.me</a>
+        designed & built by{" "}
+        <a href="https://jrson.me" target="_blank">jrson.me</a>
       </p>
       <comp.shared.iconbar />
     </footer>
--- src/_data/site.ts
+++ src/_data/site.ts
@@ -1,7 +1,6 @@
 export default {
   title: "praswicaksono.pw",
-  description:
-    "I am ordinary software engineer specialized in backend",
+  description: "I am ordinary software engineer specialized in backend",
   url: "https://praswicaksono.pw",
   shortUrl: "praswicaksono.pw",
   lang: "en",
@@ -14,5 +13,6 @@ export default {
     url: "https://github.com/praswicaksono",
   },
   twitter: "@praswicaksono",
-  ogImage: "https://og-image.vercel.app/PW.png?theme=dark&md=1&fontSize=150px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-bw-logo.svg&widths=1200&heights=630",
+  ogImage:
+    "https://og-image.vercel.app/PW.png?theme=dark&md=1&fontSize=150px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-bw-logo.svg&widths=1200&heights=630",
 } as const;
